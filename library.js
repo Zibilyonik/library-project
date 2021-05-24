f9f2d91828e9ed95fd26b7657f9bc7a0c42b2a34
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-05-24 11:08:30 +0200

    Add skeleton html

---
+++ library.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+let newBook
+let tempLibrary
+
+function Book(title, author, pages, read = false) {
+ this.title = title,
+ this.author = author,
+ this.pages = pages,
+ this.read = read
+}
+
+function addBookToLibrary() {
+  
+}
