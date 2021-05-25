3db954d80498b95ef50175491fa041963cded4f0
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-05-25 10:48:49 +0200

    -Add printbook function -Create book div

--- library.js
+++ library.js
@@ -6,8 +6,23 @@ function Book(title,author,pages,read){
   this.read = read;
 }
 
+function printBook(data) {
+  const bookcontainer = document.querySelector("#BookContainer");
+  const book = document.createElement("div");
+  book.classList.add("col row bookcontainer text-center");
+  const title = data.title;
+  const author = data.author;
+  const pages = data.pages;
+  const read = data.read;
+  
+  book.appendChild();
+  book.appendChild();
+  bookcontainer.appendChild(book);
+
+}
+
 function GetBook(){
-  const mainbook = document.querySelector('#BookContainer');
+  
   var title = document.getElementById("BookTitle").value;
   var author = document.getElementById("BookAuthor").value;
   var pages = document.getElementById("BookPages").value;
@@ -18,7 +33,4 @@ function GetBook(){
   console.log(libarray)
 }
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-  GetBook();
-});
+
