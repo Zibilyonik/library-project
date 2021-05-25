cea86dd26964ae1a4a872c5f22b32c19627775bb
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-05-25 11:52:44 +0200

    -Edits .gitignore file. -Sets the main function for createing a book from array.

--- library.js
+++ library.js
@@ -7,17 +7,30 @@ function Book(title,author,pages,read){
 }
 
 function printBook(data) {
-  const bookcontainer = document.querySelector("#BookContainer");
-  const book = document.createElement("div");
-  book.classList.add("col row bookcontainer text-center");
-  const title = data.title;
-  const author = data.author;
-  const pages = data.pages;
-  const read = data.read;
-  
-  book.appendChild();
-  book.appendChild();
-  bookcontainer.appendChild(book);
+  var arr = Object.keys(data);
+  var bookcontainer = document.querySelector("#BookContainer");
+  var book = document.createElement("div");
+  book.classList.add("col", "row", "bookcontainer", "text-center");
+  for(var i = 0; i < 4; i++){
+    var temp = document.createElement("div");
+    temp.classList.add("titlegroup", "row");
+    var temp2 = document.createElement("div");
+    temp2.classList.add("col-sm-12", "col-lg-6");
+    var temp2c = temp2.cloneNode(true);
+    var temp3 = document.createElement("h3");
+    temp3.classList.add("label", "h3");
+    var temp3c = temp3.cloneNode(true);
+    console.log(arr[i])
+    console.log(data[arr[i]])
+    temp3.innerHTML = arr[i] + " of the book:"
+    temp3c.innerHTML = data[arr[i]]
+    temp2.appendChild(temp3);
+    temp2c.appendChild(temp3c);
+    temp.appendChild(temp2);
+    temp.appendChild(temp2c);
+    book.appendChild(temp);
+    bookcontainer.appendChild(book);
+  }
 
 }
 
@@ -26,11 +39,14 @@ function GetBook(){
   var title = document.getElementById("BookTitle").value;
   var author = document.getElementById("BookAuthor").value;
   var pages = document.getElementById("BookPages").value;
-  var read = document.getElementById("BookRead").value;
+  var read = document.getElementById("BookRead").checked;
   const first = new Book(title, author, pages, read);
   console.log(first.title)
   libarray.push(first);
   console.log(libarray)
 }
 
-
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  GetBook();
+});
