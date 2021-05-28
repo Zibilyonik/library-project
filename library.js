3bbe1b3a0286fdfd23a6f973e98621bafa6adbd8
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-05-28 12:19:37 +0200

    -Fixes merge errors. -Fixes lint errors.

--- library.js
+++ library.js
@@ -75,17 +75,3 @@ const btnsubmit = document.querySelector('#Btn');
 btnsubmit.addEventListener('click', () => {
   GetBook();
 });
-let myLibrary = [];
-let newBook
-let tempLibrary
-
-function Book(title, author, pages, read = false) {
- this.title = title,
- this.author = author,
- this.pages = pages,
- this.read = read
-}
-
-function addBookToLibrary() {
-  
-}
